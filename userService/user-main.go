72d8e41bf9788d9f5f197fd041af4ad707ccdc81
j-ew-s <8515448+j-ew-s@users.noreply.github.com>
2022-06-06 19:10:51 +0100

    Fixing package

--- userService/user-main.go
+++ userService/user-main.go
@@ -2,6 +2,7 @@ package userService
 
 import (
 	"github.com/buaazp/fasthttprouter"
+	userController "github.com/j-ew-s/ms-curso-catalog-api/userService"
 )
 
 func SetRoutes(router *fasthttprouter.Router) {
